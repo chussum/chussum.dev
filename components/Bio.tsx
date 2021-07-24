eb2b88f029aa9849f5e550ed853ddf10b571c9f4
chussum <flosdor@gmail.com>
2021-07-24 16:49:40 +0900

    feat: Bio 추가

---
+++ components/Bio.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+import Link from 'next/link'
+import BLOG from '@/blog.config'
+
+const Bio = () => {
+  return (
+    <div className="bio mb-6 md:mb-16">
+      <div className="author">
+        <div className="author-description">
+          <Image
+            alt={BLOG.author}
+            width={80}
+            height={80}
+            src={`https://gravatar.com/avatar/dab63285ac3cf1d771ad62df4f853f84`}
+            className="rounded-full"
+          />
+          <div className="author-name">
+            <span className="author-name-prefix">Written by</span>
+            <Link href={'/about'}>
+              <a className="author-name-content">
+                <span>@{BLOG.author}</span>
+              </a>
+            </Link>
+            <div className="author-introduction">{BLOG.description}</div>
+            <p className="author-socials">
+              <a href="https://github.com/chussum/chussum.dev" target="_blank" rel="noreferrer">
+                <img src="https://img.shields.io/badge/GitHub-181717?style=flat-square&amp;logo=GitHub&amp;logoColor=white" />
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Bio
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps, NextPage } from 'next'
 import Container from '@/components/Container'
 import BlogPost from '@/components/BlogPost'
 import Pagination from '@/components/Pagination'
+import Bio from '@/components/Bio'
 import { getAllPosts } from '@/lib/notion'
 import BLOG from '@/blog.config'
 import { Post } from '@/types'
@@ -28,6 +29,7 @@ type Props = React.ComponentProps<typeof Pagination> & {
 const blog: NextPage<Props> = ({ postsToShow, page, showNext }) => {
   return (
     <Container title={BLOG.title} description={BLOG.description}>
+      <Bio />
       {postsToShow.map(post => (
         <BlogPost key={post.id} post={post} />
       ))}
